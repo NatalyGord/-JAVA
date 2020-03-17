41cce8dd9fa09782191b8bda0b5a5e69231fc8b9
Nataliya Gordiets <NatGord@rambler.ru>
2020-03-17 21:00:05 +0600

    //get и set пример

---
+++ src/different/diff_29.java
@@ -0,0 +1,44 @@
+//get и set пример
+package different;
+
+public class diff_29 {
+    public static void main(String[] args) {
+        class User {
+            private String firstName;
+            private String lastName;
+
+            public User() {
+                this.firstName = "";
+                this.lastName = "";
+            }
+            public void setFirstName(String firstName) {
+                if(firstName != null){
+                    this.firstName = firstName;
+                }
+            }
+            public void setLastName(String lastName) {
+                if(lastName != null){
+                    this.lastName = lastName;
+                }
+            }
+            public String getFullName() {
+                if(firstName.isEmpty() && lastName.isEmpty()){
+                    return "Unknown";
+                }
+                else{
+                    if(firstName.isEmpty()){
+                        return lastName;
+                    }
+                    else{
+                        if(lastName.isEmpty()){
+                            return firstName;
+                        }
+                        else{
+                            return (firstName + " " + lastName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
