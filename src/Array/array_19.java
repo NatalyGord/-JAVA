acda2890e99913216c7137fde9f6b774cf4a04c3
Nataliya Gordiets <NatGord@rambler.ru>
2020-03-20 00:02:11 +0600

    проверка правильности решения судоку

--- src/Array/array_19.java
+++ src/Array/array_19.java
@@ -9,94 +9,83 @@ public class array_19 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         String res = "NO";
-        int[][] arr = new int[n*n][n*n];
+        int[][] arr = new int[n * n][n * n];
         for (int j = 0; j < n * n; j++) {
             for (int i = 0; i < n * n; i++) {
                 arr[i][j] = scanner.nextInt();
             }
         }
-        System.out.println("norm");
-        int [] norm = new int[n*n];          //массив для сравнения
+        int [] norm = new int[n * n];          //массив для сравнения
         for(int i = 1; i <= n*n; i++){
             norm[i - 1] = i;
-            System.out.print(norm[i-1] + " ");
         }
+        boolean ans = true;
+        while(ans) {
+            int[] ar1 = new int[n * n];
+            for (int j = 0; j < n * n; j++) {
 
-        System.out.println();
-        System.out.println("сравнение столбцов");
-        int[] ar1 = new int[n*n];
-        for(int j = 0; j < n*n; j++) {
-            for (int i = 0; i < n * n; i++) {
-                ar1[i] = arr[j][i];               //массив из первого столбца
-                System.out.print(ar1[i] + " ");
-            }
-            Arrays.sort(ar1);
-            //сравнение массивов
-            if (Arrays.equals(norm, ar1)) {
-                System.out.println("YES");
-                res = "YES";
-            } else {
-                System.out.println("NO");
-                res = "NO";
-                break;
-            }
-        }
-        System.out.println(res);
-
-        System.out.println();
-        System.out.println("сравнение строк");
-        int[] ar2 = new int[n*n];
-        for(int j = 0; j < n*n; j++) {
-            for (int i = 0; i < n * n; i++) {
-                ar2[i] = arr[i][j];               //массив из первой строки
-                System.out.print(ar2[i] + " ");
-            }
-            Arrays.sort(ar2);
-            //сравнение массивов
-            if (Arrays.equals(norm, ar2)) {
-                System.out.println("YES");
-                res = "YES";
-            } else {
-                System.out.println("NO");
-                res = "NO";
-                break;
-            }
-        }
-        System.out.println(res);
-        System.out.println();
-
-        int v = 0;
-        int [] ar3 = new int[n*n];              //просмотр квадратов
-        for(int g = 0; g < n; g++) {
-            int a = 0;
-            int num = v;
-            for (int k = 0; k < n; k++) {
-                int c = 0;
-                int b = a;
-                for (int i = a; i < n + b; i++) {
-                    for (int j = num; j < n + num; j++) {
-                        ar3[c] = arr[j][i];
-                        System.out.print(ar3[c] + " ");
-                        c++;
-                    }
-                    a++;
+                for (int i = 0; i < n * n; i++) {
+                    ar1[i] = arr[j][i];               //массив из первого столбца
                 }
-                v++;
-                Arrays.sort(ar3);
+                Arrays.sort(ar1);
                 //сравнение массивов
-                if (Arrays.equals(norm, ar3)) {
-                    System.out.println("YES");
+                if (Arrays.equals(norm, ar1)) {
                     res = "YES";
+
                 } else {
-                    System.out.println("NO");
                     res = "NO";
+                    ans = false;
                     break;
                 }
             }
-
-            System.out.println();
-            System.out.println(a + "a");
-            System.out.println(v + "v");
+            if(ans) {
+                int[] ar2 = new int[n * n];
+                for (int j = 0; j < n * n; j++) {
+                    for (int i = 0; i < n * n; i++) {
+                        ar2[i] = arr[i][j];               //массив из первой строки
+                    }
+                    Arrays.sort(ar2);
+                    //сравнение массивов
+                    if (Arrays.equals(norm, ar2)) {
+                        res = "YES";
+                    } else {
+                        res = "NO";
+                        ans = false;
+                        break;
+                    }
+                }
+                if(ans) {
+                    int v = 0;
+                    int[] ar3 = new int[n * n];              //просмотр квадратов
+                    for (int g = 0; g < n; g++) {
+                        int a = 0;
+                        int num = v;
+                        for (int k = 0; k < n; k++) {
+                            int c = 0;
+                            int b = a;
+                            for (int i = a; i < n + b; i++) {
+                                for (int j = num; j < n + num; j++) {
+                                    ar3[c] = arr[j][i];
+                                    c++;
+                                }
+                                a++;
+                            }
+                            v++;
+                            Arrays.sort(ar3);
+                            //сравнение массивов
+                            if (Arrays.equals(norm, ar3)) {
+                                res = "YES";
+                            } else {
+                                res = "NO";
+                                ans = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            break;
         }
+        System.out.println(res);
     }
 }
