dbf32c4570e261dde9fb7026123b843556751455
Nataliya Gordiets <Natgord@rambler.ru>
2020-07-12 01:44:03 +0700

    Cat fight

---
+++ src/different/Cat.java
@@ -0,0 +1,46 @@
+package different;
+
+public class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+      int res = 0;
+      int res2 = 0;
+      if(this.age > anotherCat.age){
+        res++;
+      }
+      else{
+        if(this.age < anotherCat.age){
+          res2++;
+        }
+      }
+      if(this.weight > anotherCat.weight){
+        res++;
+      }
+      else{
+        if(this.weight < anotherCat.weight){
+          res2++;
+        }
+      }
+
+      if(this.strength > anotherCat.strength){
+        res++;
+      }
+      else{
+        if(this.strength < anotherCat.strength){
+          res2++;
+        }
+      }
+      if(res > res2){
+        return true;
+      }
+      else return false;
+    }
+    public static void main(String[] args){
+    }
+}
