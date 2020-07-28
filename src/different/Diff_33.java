c9659599c26b8ebd37f9d2b1f905d3f4a9d0ae6e
Nataliya Gordiets <Natgord@rambler.ru>
2020-07-28 12:51:57 +0700

    overloader методы

---
+++ src/different/Diff_33.java
@@ -0,0 +1,20 @@
+package different;
+
+public class Diff_33 {
+  void sum(){
+    System.out.println("0");
+  }
+  void sum(int a){
+    System.out.println(a);
+  }
+  void sum(int a, int b){
+    System.out.println(a + b);
+  }
+  void sum(int a, int b, int c){
+    System.out.println(a + b + c);
+  }
+  void sum(int a, int b, int c, int d){
+    System.out.println(a + b + c + d);
+  }
+
+}
