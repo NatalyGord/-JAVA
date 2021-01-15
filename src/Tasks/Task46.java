81183689f0393cd1a1e65bc9a7b45a421a476bfe
Nataliya Gordiets <Natgord@rambler.ru>
2021-01-15 22:29:40 +0700

    Sorting words

---
+++ src/Tasks/Task46.java
@@ -0,0 +1,39 @@
+//сортировка введеных слов в алфавитном порядке
+package Tasks;
+
+import java.io.*;
+
+public class Task46 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] array = new String[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = reader.readLine();
+        }
+
+        sort(array);
+
+        for (String word : array) {
+            System.out.println(word);
+        }
+    }
+
+    public static void sort(String[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if(isGreaterThan((array[j]), (array[j + 1]))){
+                    String st = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = st;
+                }
+            }
+        }
+    }
+
+    //Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+}
