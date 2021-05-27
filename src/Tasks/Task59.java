d890fac7602c4a2ea7e014182b4b55e9e8b47609
Natalia Gordiyets <NatGord@rambler.ru>
2021-05-27 12:48:37 +0700

    commit

---
+++ src/Tasks/Task59.java
@@ -0,0 +1,12 @@
+package Tasks;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Task59 {
+    public static void main(String[] args) {
+    }
+}
+
+
